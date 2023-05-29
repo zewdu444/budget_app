ff7c40edd1192f1c8ddeafe1f06ce697bf60c929
zewdu erkyhun <zewdu444@gmail.com>
2023-05-29 17:50:58 +0300

    Add router configuration

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :groups, only: [:index, :show, :new, :create, :destroy] do
+       resouces :expense, only: [:index, :show, :new, :create, :destroy] do
+          resources :expense_group, only: [:new, :create, :destroy]
+       end
+    end
+  end
+  get '/splash', to: 'groups#splash', as: 'splash'
+
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'groups#index', as: :unauthenticated_root
+  end
 end
