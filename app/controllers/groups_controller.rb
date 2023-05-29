ac20f35a435ef97485ede19ade46c12182a1e02f
zewdu erkyhun <zewdu444@gmail.com>
2023-05-29 21:05:20 +0300

    Add index and show page for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,13 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, except: [:splash]
-  def index; end
+  def index
+    @groups = current_user.groups
+  end
 
-  def show; end
+  def show
+    @group= Group.find(params[:id])
+    @expenses = @group.expenses.order('created_at DESC')
+  end
 
   def new; end
 
