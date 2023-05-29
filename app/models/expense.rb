5024d7db1f11fbf360c6e2e7c8d6f97a261ad5b4
zewdu erkyhun <zewdu444@gmail.com>
2023-05-29 17:05:50 +0300

    Add total expense based on group

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,4 +4,13 @@ class Expense <ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: 'User'
   has_many :expense_groups, dependent: :destroy
   has_many :groups, through: :expense_groups, dependent: :destroy
+
+   after_save :total_amount_by_group
+   after_destroy :total_amount_by_group
+
+  def total_amount_by_group(group)
+    expense_groups.where(group: group).sum(:amount)
+  end
+
+  private :total_amount_by_group
 end
