9a40f29ad31e812ae1524ab93a27c27eb8f0c17e
zewdu erkyhun <zewdu444@gmail.com>
2023-06-01 16:21:08 +0300

    Add email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'users/confirmations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
